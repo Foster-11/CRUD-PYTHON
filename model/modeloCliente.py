626bf623295bd38cfd0f10539fd1fb68abdf67a9
Foster-11 <rt.susana@hotmail.com>
2025-02-20 18:01:16 -0500

    Funcion cliente_helper realizada

--- model/modeloCliente.py
+++ model/modeloCliente.py
@@ -15,6 +15,20 @@ class modeloCliente(BaseModel):
     telefono: str
     idTipoIdentificacion: int
     fechaRegistro: datetime
-    activo: bool
+    activo: int
 
-    
+    def cliente_helper(cliente):
+        return{
+            "idCliente": str(cliente['idCliente']),
+            "primerNombre": str(cliente['primerNombre']),
+            "segundoNombre": str(cliente['segundoNombre']),
+            "primerApellido": str(cliente['primerApellido']),
+            "segundoApellido": str(cliente['segundoApellido']),
+            "email": str(cliente['email']),
+            "contrasena": str(cliente['contrasena']),
+            "direccion": str(cliente['direccion']),
+            "telefono": str(cliente['telefono']),
+            "idTipoIdentificacion": int(cliente['idTipoIdentificacion']),
+            "fechaRegistro": datetime(cliente['fechaRegistro']),
+            "activo": int(cliente['activo'])
+        }
