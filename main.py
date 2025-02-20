7b779bea10168ff0920d51d39d659480b72fde94
Foster-11 <rt.susana@hotmail.com>
2025-02-20 16:52:53 -0500

    creacion archivo main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+import uvicorn
+
+def start():
+    uvicorn.run(
+        "", ## archivo en api que contiene la funcion app
+        host="127.0.0.1",
+        port=8070,
+        reload=True
+    )
+
+if __name__ == "__main__":  
+    start()
